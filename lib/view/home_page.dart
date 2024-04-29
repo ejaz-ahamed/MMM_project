af9ef7c4ea29af0873f13ff995128725e10fa9d2
ejaz-ahamed <ahamedejaz896@gmail.com>
2024-04-29 16:41:53 +0530

    modified home page and pubspec files

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:mmm_project/core/provider/post_provider.dart';
+import 'package:mmm_project/view/details_page.dart';
+import 'package:provider/provider.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late ScrollController scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController = ScrollController();
+    scrollController.addListener(_loadMore);
+  }
+
+  void _loadMore() {
+    if (scrollController.position.pixels ==
+        scrollController.position.maxScrollExtent) {
+      Provider.of<PostProvider>(context, listen: false).fetchMore();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,14 +41,42 @@ class HomePage extends StatelessWidget {
         ),
       ),
       body: FutureBuilder(
-        future: PostProvider().getPosts(),
+        future: Provider.of<PostProvider>(context, listen: false).getPosts(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
+            final posts = Provider.of<PostProvider>(context).posts;
             return ListView.builder(
-              itemCount: snapshot.data!.length,
+              controller: scrollController,
+              itemCount: posts.length + 1,
               itemBuilder: (context, index) {
+                if (Provider.of<PostProvider>(context).isLast &&
+                    index == posts.length) {
+                  return const Center(
+                    child: Text("No More data to Load"),
+                  );
+                }
+                if (index == posts.length) {
+                  return const Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                }
                 return ListTile(
-                  title: Text(snapshot.data![index].title),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DetailsPage(
+                            postId: posts[index].id,
+                          ),
+                        ));
+                  },
+                  leading: CircleAvatar(
+                    child: Text(posts[index].id.toString()),
+                  ),
+                  title: Text(posts[index].title),
                 );
               },
             );
