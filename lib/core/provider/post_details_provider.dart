c9b6b498eba32d48aeb34bf23ae139e96f92c510
ejaz-ahamed <ahamedejaz896@gmail.com>
2024-04-29 16:42:31 +0530

    created a postdetails provider

---
+++ lib/core/provider/post_details_provider.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:mmm_project/core/services/api_services.dart';
+import 'package:mmm_project/model/comment_model.dart';
+import 'package:mmm_project/model/post_model.dart';
+
+class PostDetailsProvider extends ChangeNotifier {
+  PostModel? _post;
+  PostModel? get post {
+    return _post;
+  }
+
+  final List<CommentModel> _comments = [];
+  List<CommentModel> get comments {
+    return _comments;
+  }
+
+  Future<void> getPostDetails(int postId) async {
+    try {
+      final post = await ApiServices.getPostDetails(postId);
+      _post = post;
+      notifyListeners();
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<void> getComments(int postId) async {
+    try {
+      final comments = await ApiServices.getComments(postId);
+      _comments.addAll(comments);
+      notifyListeners();
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _post = null;
+    _comments.clear();
+    notifyListeners();
+  }
+}
