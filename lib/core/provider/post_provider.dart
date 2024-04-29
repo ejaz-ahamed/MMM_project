07ed796efd4ac4a9227d4090e4ef2ad88921c325
ejaz-ahamed <ahamedejaz896@gmail.com>
2024-04-29 16:44:47 +0530

    modified post provider

--- lib/core/provider/post_provider.dart
+++ lib/core/provider/post_provider.dart
@@ -3,7 +3,33 @@ import 'package:mmm_project/core/services/api_services.dart';
 import 'package:mmm_project/model/post_model.dart';
 
 class PostProvider extends ChangeNotifier {
+  final List<PostModel> _list = [];
+  int page = 1;
+  bool _isLast = false;
+
+  bool get isLast {
+    return _isLast;
+  }
+
+  List<PostModel> get posts {
+    return _list;
+  }
+
   Future<List<PostModel>> getPosts() async {
-    return await ApiServices.getPosts();
+    final posts = await ApiServices.getPosts(1);
+    _list.addAll(posts);
+    notifyListeners();
+    return _list;
+  }
+
+  Future<List<PostModel>> fetchMore() async {
+    page++;
+    final posts = await ApiServices.getPosts(page);
+    if (posts.length < 20) {
+      _isLast = true;
+    }
+    _list.addAll(posts);
+    notifyListeners();
+    return _list;
   }
 }
--- lib/core/services/api_services.dart
+++ lib/core/services/api_services.dart
@@ -1,17 +1,21 @@
 import 'dart:developer';
 
 import 'package:dio/dio.dart';
+import 'package:mmm_project/model/comment_model.dart';
 import 'package:mmm_project/model/post_model.dart';
 
 class ApiServices {
-  static final dio = Dio();
+  static final dio =
+      Dio(BaseOptions(baseUrl: "https://jsonplaceholder.typicode.com"));
 
-  static Future<List<PostModel>> getPosts() async {
-    const baseUrl = 'https://jsonplaceholder.typicode.com/posts';
+  static Future<List<PostModel>> getPosts(int page) async {
+    final postPath = '/posts?_page=$page&_limit=20';
     try {
-      final response = await dio.get(baseUrl);
+      final response = await dio.get(postPath);
       if (response.statusCode == 200) {
-        final posts = response.data;
+        final posts = (response.data as List)
+            .map((json) => PostModel.fromJson(json))
+            .toList();
         return posts;
       } else {
         throw Exception('Cannot fetch posts .Try again');
@@ -19,6 +23,26 @@ class ApiServices {
     } catch (e) {
       log(e.toString());
       throw Exception(e.toString());
+    }
+  }
+
+  static Future<PostModel> getPostDetails(int postId) async {
+    try {
+      final response = await dio.get('/posts/$postId');
+      return PostModel.fromJson(response.data);
+    } on DioException catch (e) {
+      throw e.message ?? "Something went wrong, please try again.";
+    }
+  }
+
+  static Future<List<CommentModel>> getComments(int postId) async {
+    try {
+      final response = await dio.get('/posts/$postId/comments');
+      final comments =
+          (response.data as List).map((e) => CommentModel.fromJson(e)).toList();
+      return comments;
+    } on DioException catch (e) {
+      throw e.message ?? "Something went wrong, please try again.";
     }
   }
 }
